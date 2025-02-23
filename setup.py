fd6a5ae06c59280f7501f99e7bd296a536b10eda
Sai <7330692+saiushiv@users.noreply.github.com>
2025-02-23 08:03:09 -0500

    added installation setup.

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
+#!/usr/bin/env python3
+
 from setuptools import setup, find_packages
 
 setup(
-    name="geofetch_cli_util",
-    version="1.0",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    name='geofetch_cli_util',
+    version='1.0',
+    packages=find_packages(),
     install_requires=[
         'requests==2.32.3',
     ],
     entry_points={
-        "console_scripts": [
-            "geofetch=src.cli:main",  # This makes the CLI available as a command
+        'console_scripts': [
+            'geofetch=src.cli:main',  # This makes the CLI available as a command
         ],
     },
 )
